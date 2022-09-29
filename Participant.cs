283a893053bbccedec7934ef0c2beeeecb90c75d
Håkan Sjö Ballina <hakan@badpixel.se>
2022-09-29 15:05:04 +0200

    Demo code

---
+++ Participant.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+
+namespace Link;
+
+public class Participant
+{
+    public Participant()
+    {
+        Name = string.Empty;
+        PhoneNumber = string.Empty;
+        StartNumber = string.Empty;
+    }
+
+    public string Name { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public bool HasCheated { get; set; }
+    public string PhoneNumber { get; set; }
+    public string StartNumber { get; set; }
+
+    public DateTime TheTime => new((EndTime - StartTime).Ticks);
+
+    public override string ToString()
+        => $"{StartNumber.PadRight(10)}{Name.PadRight(30)}{TheTime:T}\t{StartTime:T}\t{EndTime:T}\t{DidCheat}\t Phone: {PhoneNumber}";
+
+    private string DidCheat => HasCheated ? "Cheated" : "Fair";
+}
+
+public static class ParticipantBuilder
+{
+    public static IEnumerable<Participant> Build()
+    {
+        var participants = new Faker<Participant>();
+
+        participants
+            .RuleFor(p => p.Name, f => f.Name.FullName())
+            .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber())
+            .RuleFor(p => p.StartTime,
+                f => f.Date.Between(new DateTime(2022, 09, 01, 10, 0, 0), new DateTime(2022, 09, 01, 11, 0, 0)))
+            .RuleFor(p => p.EndTime,
+                f => f.Date.Between(new DateTime(2022, 09, 01, 13, 0, 0), new DateTime(2022, 09, 01, 14, 0, 0)))
+            .RuleFor(p => p.HasCheated, f => f.PickRandom(true, false))
+            .RuleFor(p => p.StartNumber, f => f.Random.Number(99999).ToString());
+
+        return participants.GenerateBetween(50, 100);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Link;
+
+internal class Program
+{
+    private static readonly IEnumerable<Participant> Participants = ParticipantBuilder.Build().ToList();
+
+    static void Main(string[] args)
+    {
+        var top5 = Participants
+            .Where(x => !x.HasCheated)
+            .OrderBy(p => p.TheTime)
+            .Take(5);
+
+        var top5Cheeters = Participants
+            .Where(x => x.HasCheated)
+            .OrderBy(p => p.TheTime)
+            .Take(5);
+
+        var combined = Participants
+            .GroupBy(p
+                    => p.HasCheated,
+                (ch, part)
+                    => new
+                    {
+                        ch,
+                        Peeps = part
+                            .OrderBy(p => p.TheTime)
+                            .Take(5)
+                    });
+
+        foreach (var x1 in combined)
+        {
+            Console.WriteLine(x1.ch ? "Cheaters" : "Fair Players");
+            PrintEm(x1.Peeps);
+        }
+        Console.WriteLine("\n-----------------\n\n");
+        PrintEm(top5);
+        Console.WriteLine("\n-----------------\n\n");
+        PrintEm(top5Cheeters);
+        Console.WriteLine("\n-----------------\n\n");
+
+        // if condition with assignment (both do the same thing)
+        Participant? participant;
+
+        if ((participant = Participants.FirstOrDefault()) is null)
+        {
+            Console.WriteLine("Couldn't find that");
+        }
+        else
+        {
+            Console.WriteLine($"Found {participant}");
+        }
+
+        Console.WriteLine((participant = Participants
+            .FirstOrDefault(p => p.Name == "xxx")) is null
+            ? "Couldn't find that"
+            : $"Found {participant}");
+    }
+
+    private static void PrintEm(IEnumerable<Participant> peeps)
+    {
+        foreach (var p in peeps)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
